87e2a445690549c1f8d2d71730fea6fc7949f08f
Said Mariano <smariano170@gmail.com>
2025-07-04 10:14:15 -0600

    feat: Script para exortar datos limpios

---
+++ sql/exportacion_datos.sql
@@ -0,0 +1,15 @@
+-- Cantidad de registros
+SELECT
+	COUNT(*) AS cantidad_registros
+FROM
+	amazon;
+    
+-- Cantidad de registros: 43644
+
+-- Datos a exportar
+SELECT
+	*
+FROM
+	amazon;
+    
+-- Dar click en la parde que dice Export
